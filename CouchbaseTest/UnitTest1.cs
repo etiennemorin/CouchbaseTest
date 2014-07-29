2fd1196b4491adacd220bb529d7a8e337c4e0087
Etienne Morin <etienne.morin@boldradius.com>
2014-07-29 15:36:24 -0400

    Fixing stuff

---
+++ CouchbaseTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Couchbase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CouchbaseTest
+{
+    [TestClass]
+    public class CouchbaseClientTests
+    {
+        [TestMethod]
+        public void InstantiateClient()
+        {
+            var client = new CouchbaseClient();
+        }
+    }
+}
